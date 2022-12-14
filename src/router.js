41fa94931c0609b782b14fdc7dcc717eea21f028
reiko_iguchi <reiko6122@gmail.com>
2022-12-14 22:51:50 +0900

    2022/12/14

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: "Home",
+    component: Home
+  },
+  {
+    path: '/about/:city',
+    name: "About",
+    component: About,
+    props: true,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
